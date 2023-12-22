d7b958162be6a19b68ab2e45780f7a298f87f220
bskuts <93197518+bskuts@users.noreply.github.com>
2023-12-22 19:46:30 +0800

    Add files via upload

---
+++ admin/sql.php
@@ -0,0 +1,8 @@
+<?php
+    $SERVER = "localhost";
+    $USER = "root";
+    $PASSWORD = "";
+    $DATABASE = "2101020026_Baskara";
+
+    $con = mysqli_connect($SERVER, $USER, $PASSWORD, $DATABASE);
+?>
